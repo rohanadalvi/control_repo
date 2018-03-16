6b2ea6f28456a40ff4102e0ceab003a197f6344e
rohanadalvi <rohan.dalvi@capgemini.com>
2018-03-15 23:26:29 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,6 +3,8 @@ node default {
   file { '/root/README':
      ensure => file,
      content => 'this is REad Me',
-     owner   => 'root',
      }
+   file { '/root/README':4
+     owner   => 'root',
+                 }
 }
