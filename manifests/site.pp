9a371107c43598569f5f16fb8919e50966114be5
rohanadalvi <rohan.dalvi@capgemini.com>
2018-03-20 23:54:41 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,6 @@
 
 node default {
-  file { '/root/README':
-     ensure => file,
-     content => 'this is REad Me',
-     }
-   file { '/root/README':4
-     owner   => 'root',
-                 }
 }
+node 'master.puppet.vm' {
+  include role::master_server
+ }
